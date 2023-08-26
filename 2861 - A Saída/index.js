b8d5635d956648db30defdd625b1492ee6e0d2ac
Pedro Siqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2023-08-26 20:07:20 -0300

    Exercicios

---
+++ 2861 - A Saída/index.js
@@ -0,0 +1,8 @@
+var input = require("fs").readFileSync("stdin", "utf8");
+var lines = input.split("\n");
+
+const vezes = +lines.shift();
+
+for (let i = 0; i < vezes; i++) {
+  console.log("gzuz");
+}
