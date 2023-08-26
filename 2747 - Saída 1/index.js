1c16293cc5abc79d01895be181c711295d58d9fe
Pedro Siqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2023-08-25 23:45:28 -0300

    Exercicios

---
+++ 2747 - Saída 1/index.js
@@ -0,0 +1,10 @@
+var input = require("fs").readFileSync("stdin", "utf8");
+var lines = input.split("\n");
+
+console.log("-".repeat(39));
+console.log("|" + " ".repeat(37) + "|");
+console.log("|" + " ".repeat(37) + "|");
+console.log("|" + " ".repeat(37) + "|");
+console.log("|" + " ".repeat(37) + "|");
+console.log("|" + " ".repeat(37) + "|");
+console.log("-".repeat(39));
