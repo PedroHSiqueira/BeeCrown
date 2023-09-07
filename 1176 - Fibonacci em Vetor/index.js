586ecf6d56b876d861bbdd3731659235a3ce5830
Pedro Siqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2023-09-06 22:54:54 -0300

    Exercicios

--- 1176 - Fibonacci em Vetor/index.js
+++ 1176 - Fibonacci em Vetor/index.js
@@ -1,8 +1,25 @@
-var input = require('fs').readFileSync('stdin', 'utf8');
-var lines = input.split('\n');
+var input = require("fs").readFileSync("stdin", "utf8");
+var lines = input.split("\n");
 
-let vezes = lines.shift()
-vezes = +vezes
+let numero = parseInt(lines.shift());
+let temp = 1;
+let soma = 0;
+let anterior = 0;
 
-console.log(lines)
-console.log(vezes);
+for (let i = 0; i < numero; i++) {
+  let atual = parseInt(lines.shift());
+
+  for (let j = 1; j < atual; j++) {
+    soma = temp + anterior;
+    anterior = temp;
+    temp = soma;
+  }
+  if (atual == 1) {
+    console.log(`Fib(${1}) = ${1}`);
+  } else {
+    console.log(`Fib(${atual}) = ${soma}`);
+  }
+  soma = 0;
+  temp = 1;
+  anterior = 0;
+}
